df9aaa63b0153c680e4fdc08461c4f207ef41ef5
rln-tomas <tomas.ezequiel.rln@gmail.com>
2022-01-28 21:15:33 -0300

    App component and entry point

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react"
+
+import {Button} from 'antd'
+import './App.css'
+
+export const App: FC = () => {
+    return (
+    <div>
+        <Button type='primary'>Button</Button>
+    </div>)
+}
